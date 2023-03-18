5ba89d3a56b01fe26dbddbf68ab9a006b1bd4174
Finn <34718806+FinnDore@users.noreply.github.com>
2023-03-18 21:15:47 +0000

    fixed build

--- src/server/api/routers/example.ts
+++ src/server/api/routers/example.ts
@@ -14,11 +14,6 @@ export const exampleRouter = createTRPCRouter({
                 greeting: `Hello ${input.text}`,
             };
         }),
-
-    getAll: publicProcedure.query(({ ctx }) => {
-        return ctx.prisma.example.findMany();
-    }),
-
     getSecretMessage: protectedProcedure.query(() => {
         return 'you can now see this secret message!';
     }),
