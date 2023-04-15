95fc9daca29512205492800ac435bfe7fdb06273
Finn <34718806+FinnDore@users.noreply.github.com>
2023-04-15 22:34:36 +0100

    umami

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,6 +11,12 @@ export default function Document() {
             <title>Vote</title>
             <Head>
                 <link rel="icon" href="/favicon.ico" />
+                <script
+                    async
+                    defer
+                    data-website-id="2b7c5ed7-8f27-4b03-bdab-622ef35a1e33"
+                    src="https://umami.finndore.dev/umami.js"
+                ></script>
             </Head>
             <body className="h-screen bg-white text-black dark:bg-black dark:text-white">
                 <Main />
