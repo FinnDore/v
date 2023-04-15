2e1513545a07ee30676b5586c295d02e0d7121e5
Finn <34718806+FinnDore@users.noreply.github.com>
2023-04-15 13:32:43 +0100

    updated create button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,14 @@ import { type NextPage } from 'next';
 import Link from 'next/link';
 import { signOut } from 'next-auth/react';
 
+import { Button } from '@/components/button';
+
 const Home: NextPage = () => {
     return (
         <div className="mx-auto my-auto flex h-max w-max max-w-full place-items-center gap-12 px-12 py-6 lg:max-w-screen-lg">
-            <Link href="/create">Create Vote</Link>
+            <Link href="/create">
+                <Button variant="outline">Create Vote </Button>
+            </Link>
 
             <button onClick={() => void signOut()}>Logout</button>
         </div>
