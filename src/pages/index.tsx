5d430372851849f010aaf8699cd0e03b41765315
Finn <34718806+FinnDore@users.noreply.github.com>
2023-07-01 20:26:29 +0300

    stoof

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,32 @@
 import { type NextPage } from 'next';
-import Link from 'next/link';
-import { signOut } from 'next-auth/react';
-
-import { Button } from '@/components/button';
+import Balancer, { Provider } from 'react-wrap-balancer';
 
 const Home: NextPage = () => {
     return (
-        <div className="mx-auto my-auto flex h-max w-max max-w-full place-items-center gap-12 px-12 pb-6 lg:max-w-screen-lg">
-            <Link href="/create">
-                <Button variant="outline">Create Vote </Button>
-            </Link>
-
-            <button onClick={() => void signOut()}>Logout</button>
-        </div>
+        <Provider>
+            <div className="absolute -z-10 h-screen w-screen object-cover">
+                <picture className="h-screen w-screen object-cover">
+                    <img
+                        src="/temp-rays.png"
+                        className=" h-screen w-screen object-cover"
+                    />
+                </picture>
+            </div>
+            <div className="mx-auto flex h-max w-max max-w-full flex-col place-items-center  px-12 pb-6 lg:max-w-screen-lg">
+                <h1 className="text-2xl font-bold md:mt-16 md:text-6xl">
+                    <Balancer>The better way to point things</Balancer>
+                </h1>
+                <h2 className="mt-6 text-center text-gray-400">
+                    <Balancer>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Pellentesque vitae quam ac eros malesuada molestie ac
+                        sit amet tortor. Aenean vehicula dignissim dui, et
+                        aliquet purus porttitor sed. Nullam hendrerit orci eget
+                        aliquet tempor
+                    </Balancer>
+                </h2>
+            </div>
+        </Provider>
     );
 };
 
