03955dc932bcddaed12e0c95d9b5c201144b158b
Finn <34718806+FinnDore@users.noreply.github.com>
2023-07-09 21:36:14 +0300

    more cleanup

--- src/components/landing/particles.tsx
+++ src/components/landing/particles.tsx
@@ -46,7 +46,7 @@ export const Particles: React.FC<ParticlesProps> = ({
     const canvasRef = useRef<HTMLCanvasElement>(null);
     const canvasContainerRef = useRef<HTMLDivElement>(null);
     const context = useRef<CanvasRenderingContext2D | null>(null);
-    const circles = useRef<any[]>([]);
+    const circles = useRef<Circle[]>([]);
     const mousePosition = MousePosition();
     const mouse = useRef<{ x: number; y: number }>({ x: 0, y: 0 });
     const canvasSize = useRef<{ w: number; h: number }>({ w: 0, h: 0 });
@@ -292,9 +292,19 @@ export const Particles: React.FC<ParticlesProps> = ({
         initCanvas();
         return () => {
             circles.current = [];
-            console.log('reset');
         };
-    }, [initCanvas, refresh]);
+    }, [
+        initCanvas,
+        refresh,
+        circles,
+        images,
+        quantity,
+        staticity,
+        vx,
+        vy,
+        ease,
+        color,
+    ]);
 
     useEffect(() => {
         if (canvasRef.current) {
