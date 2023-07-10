7e9aaaa2bc6b18abc9101bb04325bed11b294c5c
Finn <34718806+FinnDore@users.noreply.github.com>
2023-07-10 16:53:32 +0300

    rip sam

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,8 @@ export default {
                 'fade-in': 'fade-in .25s linear forwards',
                 'pulse-size':
                     'pulseSize 1s cubic-bezier(0, 0, 0.2, 1) infinite',
+                'drop-in':
+                    'dropIn .5s cubic-bezier(0.68, -0.55, 0.27, 1.55) forwards',
             },
         },
     },
